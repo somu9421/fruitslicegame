b51cdac30719ea6dcf8d95d2ee987b509a290d31
Somesh Punpaliya <110421711+somu9421@users.noreply.github.com>
2024-07-17 18:27:07 +0530

    Update jav.js

--- jav.js
+++ jav.js
@@ -1,182 +1,105 @@
-var playing = false;
-var score;
-var trialsLeft;
-var step;
-var step2;
-var action; //used for setInterval
-var fruits = ['apple', 'banana', 'cherry', 'grapes', 'mango', 'orange', 'lemon', 'guava', 'watermelon','pears','pineapple','strawberry','bomb'];
-var bomb = ['bomb'];
-$(function(){
-    
-$("#startbut").click(function(){
-   
-if (playing==true){
+$(function() {
+    var playing = false;
+    var score;
+    var trialsLeft;
+    var step;
+    var step2;
+    var action; 
+    var action2; 
+    var fruits = ['apple', 'banana', 'cherry', 'grapes', 'mango', 'orange', 'lemon', 'guava', 'watermelon', 'pears', 'pineapple', 'strawberry'];
 
-location.reload();
+    $("#startbut").click(function() {
+        if (playing) {
+            location.reload();
+        } else {
+            playing = true;
+            score = 0;
+            $("#scorevalue").html(score);
+            $("#trialsleft").show();
+            trialsLeft = 3;
+            addHearts();
+            $("#gameover").hide();
+            $("#startbut").html("Reset Game");
+            startAction();
+            startAction2();
+        }
+    });
 
-}else{
+    $("#fruit1").mouseover(function() {
+        score++;
+        $("#scorevalue").html(score);
+        document.getElementById("slicesound").play(); 
+        clearInterval(action); 
+        $("#fruit1").hide("explode", { pieces: 3 }, 500); 
+        setTimeout(startAction, 1000); 
+    });
 
-playing = true;
+    $("#bombb").mouseover(function() {
+        score--;
+        $("#scorevalue").html(score); 
+        document.getElementById("bombsound").play(); 
+        clearInterval(action2); 
+        $("#bombb").hide("explode", { pieces: 3 }, 500); 
+        setTimeout(startAction2, 1000); 
+    });
 
-score=0;
-$("#scorevalue").html(score);
-
-$("#trialsleft").show();
-trialsleft=3;
-addhearts();
-
-$("#gameover").hide();
-
-$("#startbut").html("reset game");
-
-startAction();
-startaction2();
-}
-});
-
-    
-//slice a fruit
-    
-$("#fruit1").mouseover(function(){
-    score++;
-    
-    $("#scorevalue").html(score); //update score
-   document.getElementById("slicesound").play();
-    $("#slicesound")[0].play();//play sound
-    
-    //stop fruit
-    clearInterval(action);
-    
-    //hide fruit
-    $("#fruit1").hide("explode",{pieces : 3}, 500); //slice fruit
-    
-    //send new fruit
-    
-    setTimeout(startAction, 500);
-});
-
-$("#bombb").mouseover(function(){
-    score--;
-    //trialsLeft--;
-    $("#scorevalue").html(score); //update score
-   document.getElementById("bombsound").play();
-    $("#bombsound")[0].play();//play sound
-    
-    //stop fruit
-    clearInterval(action2);
-    
-    //hide fruit
-    $("#bombb").hide("explode",{pieces : 3} ,500); //slice fruit
-    
-    //send new fruit
-    setTimeout(startaction2, 1000);
-});
-
-
-
-
-function addhearts(){
-    $("#trialsleft").empty();
-    for(i=0;i<trialsleft;i++){
-        $("#trialsleft").append('<img src="heart2-removebg-preview.png" class="life" >');
+    function addHearts() {
+        $("#trialsleft").empty();
+        for (var i = 0; i < trialsLeft; i++) {
+            $("#trialsleft").append('<img src="heart2-removebg-preview.png" class="life">');
+        }
     }
-}
 
-function startaction2(){
-
-
-$("#bombb").show();
-step2 =  1+Math.round(1*Math.random());
-$("#bombb").css({'left' : Math.round(1200*Math.random()),'top' : -50});
-
-action2=setInterval(function(){
-
-    $("#bombb").css('top', $("#bombb").position().top + step);  
-
-    //if($("#bombb").position().top > $("#maincontainer2").height()){
-        charm=Math.round(1000*Math.random());
-        if(charm==2){
-    if(trialsleft > 1){
-     
-        $("#bombb").show();
-        $("#bombb").css({'left' : Math.round(1200*Math.random),'top' : -50});   
-
-       // step2 = 2+ Math.round(2*Math.random);
-
-     //   trialsleft --;
-      // addhearts();
-        startaction2(); 
-    
-
+    function startAction2() {
+        if (Math.random() > 0.3) { 
+            $("#bombb").show();
+            step2 = 3 + Math.round(3 * Math.random());
+            $("#bombb").css({ 'left': Math.round(1200 * Math.random()), 'top': -50 });
+            action2 = setInterval(function() {
+                $("#bombb").css('top', $("#bombb").position().top + step2);
+                if ($("#bombb").position().top > $("#maincontainer2").height()) {
+                    // $("#bombb").hide();
+                    clearInterval(action2);
+                    startAction2();
+                }
+            }, 22);
+        }
     }
-}
-   }, 25);
-}
 
-
-function startAction(){
-
-   $("#fruit1").show();
-   choosefruit();
-   $("#fruit1").css({'left' : Math.round(1200*Math.random()),'top' : -50});
-
-   step =  3+Math.round(4*Math.random());
-
-   action=setInterval(function(){
-
-    $("#fruit1").css('top', $("#fruit1").position().top + step);  
-
-    if($("#fruit1").position().top >= $("#maincontainer2").height()){
-
-    if(trialsleft > 1){
-     
+    function startAction() {
         $("#fruit1").show();
-        choosefruit();
-        $("#fruit1").css({'left' : Math.round(1200*Math.random),'top' : -50});   
-
-        //step = 5+ Math.round(3*Math.random);
-
-        trialsleft --;
-
-        addhearts();
-        startAction();
-    }else{
-     playing = false;
-     $("#startbut").html("Start Game"); // change button to Start Game
-     $("#gameover").html('<p>Game Over!</p><p>Your score is '+ score +'</p>');
-     $("#gameover").show();
-     $("#trialsleft").hide();
-     stopaction();
-     action3=setInterval(function(){
-        location.reload();
-     },8000);
+        chooseFruit();
+        $("#fruit1").css({ 'left': Math.round(1200 * Math.random()), 'top': -50 });
+        step = 4+ Math.round(5 * Math.random());
+        action = setInterval(function() {
+            $("#fruit1").css('top', $("#fruit1").position().top + step);
+            if ($("#fruit1").position().top > $("#maincontainer2").height()) {
+                if (trialsLeft > 1) {
+                    trialsLeft--;
+                    addHearts();
+                    // $("#fruit1").hide();
+                    clearInterval(action);
+                    startAction();
+                } else {
+                    playing = false;
+                    $("#startbut").html("Start Game");
+                    $("#gameover").html('<p>Game Over!</p><p>Your score is ' + score + '</p>');
+                    $("#gameover").show();
+                    $("#trialsleft").hide();
+                    stopAction();
+                }
+            }
+        }, 20);
     }
+
+    function chooseFruit() {
+        $("#fruit1").attr('src', 'images/' + fruits[Math.floor(Math.random() * fruits.length)] + '.png');
     }
-   }, 20);
-}
 
-
-
-
-
-
-function choosefruit(){
-$("#fruit1").attr('src' , 'images/' + fruits[Math.round(11*Math.random())] +'.png');
-}
-
-
-
-function stopaction(){
-  clearInterval(action);
-  clearInterval(action2);
-  $("#fruit1").hide();
-  $("#bombb").hide();
-}
-
-
-
-
-
-
-
+    function stopAction() {
+        clearInterval(action);
+        clearInterval(action2);
+        $("#fruit1").hide();
+        $("#bombb").hide();
+    }
 });
